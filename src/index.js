c4205e0b44388505de396374bd5ce14dcc2020ec
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-03-18 20:36:47 +0200

    Зміни до ДЗ11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -11,19 +12,19 @@ const loadMoreBtn = document.querySelector('.load-more');
 let page = 1;
 
 form.addEventListener('submit', onSearch);
-loadMoreBtn.style.display = 'none';
 loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+loadMoreBtn.style.display = 'none';
 
 function onSearch(e) {
     e.preventDefault();
 
     page = 1;
-    gallery = innerHTML = '';
+    gallery.innerHTML = '';
 
-    const name = input.value.trim();
+    const query = input.value.trim();
 
-    if (name !== '') {
-        pixabay(name);
+    if (query !== '') {
+        pixabay(query);
         
     } loadMoreBtn.style.display = 'none';
 
@@ -33,18 +34,18 @@ function onSearch(e) {
 }
 
 function onLoadMoreBtn() {
-    const name = input.value.trim();
+    const query = input.value.trim();
     page += 1;
-    pixabay(name, page);
+    pixabay(query, page);
 }
 
-async function pixabay(name, page) {
+async function pixabay(query, page) {
     const API_URL = 'https://pixabay.com/api/';
 
     const options = {
         params: {
             key: '34459623-b0ea211fe3985b98adc104771',
-            q: name,
+            q: query,
             image_type: 'photo',
             orientation: 'horizontal',
             safesearch: 'true',
@@ -64,5 +65,64 @@ async function pixabay(name, page) {
         createMarkup(response.data);
     } catch (error) {
         console.log(error);
+    }
+}
+
+function createMarkup(array) {
+    const markup = array.hits
+        .map(item =>
+            `<a class="photo-link" href="${item.largeImageURL}">
+            <div class="photo-card">
+            <div class="photo">
+            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
+            </div>
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            ${item.likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            ${item.views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            ${item.comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            ${item.downloads}
+                        </p>
+                    </div>
+            </div>
+        </a>`
+        
+    ).join('');
+    gallery.insertAdjacentElement('beforeend', markup);
+    simpleLightbox.refresh();
+}
+
+const simpleLightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+function notification(length, totalHits) {
+    if (length === 0) {
+        Notiflix.Notify.failure(
+'Sorry, there are no images matching your search query. Please try again.'
+    );
+        return;
+    }
+
+    if (page === 1) {
+        loadMoreBtn.style.display = 'flex';
+
+Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+
+    if (length < 40) {
+        loadMoreBtn.style.display = 'none';
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
     }
 }
